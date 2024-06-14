6a8087bf8c95a728846e8d29211b5d4851f47917
KadirToptas <kdrrtptss@gmail.com>
2024-06-14 17:20:34 +0300

    latest

--- Assets/Scripts/Ball_Controller.cs
+++ Assets/Scripts/Ball_Controller.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SubsystemsImplementation;
+using UnityEngine.UI;
 
 public class Ball_Controller : MonoBehaviour
 {
+    private GameManager gm;
     public Rigidbody rb;
+    public Image levelBar;
 
     private Vector2 _firstPos;
     private Vector2 _secondPos;
@@ -17,13 +20,20 @@ public class Ball_Controller : MonoBehaviour
     public float currentGroundNumber;
     void Start()
     {
-        
+        gm = GameObject.FindObjectOfType<GameManager>();
     }
 
     
     void Update()
     {
         Swipe();
+        levelBar.fillAmount = currentGroundNumber / gm.groundNumbers;
+
+        if (levelBar.fillAmount == 1)
+        {
+            gm.LevelUpdate();
+        }
+        
     }
 
     private void Swipe()
